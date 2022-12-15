f0e19418324d088ceab19f7a1289e064e859a850
Peter Kudela <peter.kudela@ilogics.com>
2022-12-15 14:40:58 +0100

    VetDTO controller

--- monolith/src/main/java/org/springframework/samples/petclinic/vet/service/VetService.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/vet/service/VetService.java
@@ -1,10 +1,14 @@
 package org.springframework.samples.petclinic.vet.service;
 
+import org.monolithic.petclinic.dto.VetDTO;
+import org.monolithic.petclinic.model.NamedEntity;
 import org.springframework.samples.petclinic.vet.db.VetRepository;
 import org.springframework.samples.petclinic.vet.model.Vet;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class VetService {
@@ -19,5 +23,16 @@ public class VetService {
         return this.vets.findAll();
     }
 
+    public List<VetDTO> allVetsDto() {
+        return this.vets.findAll().stream().map(this::toDto).collect(Collectors.toList());
+    }
+
+    private VetDTO toDto(final Vet vet) {
+        VetDTO vetDTO = new VetDTO();
+        vetDTO.setFirstName(vet.getFirstName());
+        vetDTO.setLastName(vet.getLastName());
+        vetDTO.setSpecialties(vet.getSpecialties().stream().map(NamedEntity::getName).collect(Collectors.toSet()));
+        return vetDTO;
+    }
 
 }
