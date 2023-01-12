0bc368f8532f829c815641aae2c32dc084f77db9
Peter Kudela <peter.kudela@ilogics.com>
2023-01-12 14:28:47 +0100

    Vet MicroService split

--- monolith/src/main/java/org/springframework/samples/petclinic/owner/controller/VetController.java
+++ monolith/src/main/java/org/springframework/samples/petclinic/owner/controller/VetController.java
@@ -13,9 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.samples.petclinic.vet.controller;
+package org.springframework.samples.petclinic.owner.controller;
 
-import org.springframework.samples.petclinic.vet.service.VetService;
+import org.springframework.samples.petclinic.owner.service.vet.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
---
+++ monolith/src/main/java/org/springframework/samples/petclinic/owner/service/vet/VetService.java
@@ -0,0 +1,25 @@
+package org.springframework.samples.petclinic.owner.service.vet;
+
+import org.monolithic.petclinic.dto.VetDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.owner.service.VetRestClient;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VetService {
+
+    @Autowired
+    private VetRestClient vetRestClient;
+
+    public VetService(final VetRestClient vetRestClient) {
+        this.vetRestClient = vetRestClient;
+    }
+
+
+    public List<VetDTO> allVetsDto() {
+        return vetRestClient.allVetsDto();
+    }
+
+}
