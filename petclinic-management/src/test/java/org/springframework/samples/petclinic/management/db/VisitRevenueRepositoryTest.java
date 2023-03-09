e0d4adcecb586381cd40d9a4c907688747161212
Peter Kudela <peter.kudela@ilogics.com>
2023-03-09 10:55:22 +0100

    tests adjusted

---
+++ petclinic-management/src/test/java/org/springframework/samples/petclinic/management/db/VisitRevenueRepositoryTest.java
@@ -0,0 +1,26 @@
+package org.springframework.samples.petclinic.management.db;
+
+import org.junit.jupiter.api.Test;
+import org.monolithic.petclinic.dto.YearlyRevenueDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.samples.petclinic.management.db.VisitRevenueRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class VisitRevenueRepositoryTest {
+
+    @Autowired
+    VisitRevenueRepository visitRevenueRepository;
+
+    @Test
+    void testGenerateRevenueReport() {
+        List<YearlyRevenueDTO> yearlyRevenues = this.visitRevenueRepository.listYearlyRevenue();
+        assertThat(yearlyRevenues).hasSize(1);
+        assertThat(yearlyRevenues.get(0).getTotal()).isEqualTo(800L);
+    }
+
+}
